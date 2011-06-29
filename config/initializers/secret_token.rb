4e58211c3033796b32a16b02bce1a8d8ef3d2794
Aaron Smith <aaron.smith@hertford.ox.ac.uk>
2011-06-29 22:24:15 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SiamWebsite::Application.config.secret_token = 'a6351624af2788d27d8ad72ba4e5a2f0df5d20d1ef006e0580f6b1e89d74f2748281c076d65bbca0960758755cd80191432754b7f99b390738db06a476657a1f'
